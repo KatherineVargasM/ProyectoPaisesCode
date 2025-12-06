12fd9737fe875299c297a65dc39b675f15fab194
KatherineVargasM <katherinevm27@uniandes.edu.ec>
2025-12-06 12:58:13 -0500

    commit1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { Paises } from './paises/paises';
+//import { NuevoPais } from './paises/nuevo-pais/nuevo-pais';
+import { Provincias } from './provincias/provincias';
+import { Cantones } from './cantones/cantones';
+import { Direcciones } from './direcciones/direcciones';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Dashboard,
+        pathMatch: 'full'
+    },
+    {
+        path:'paises',
+        component: Paises,
+    }, 
+    //{
+       // path:"nuevopais",
+        //component: NuevoPais,
+        //pathMatch: 'full'
+    //},
+   {
+        path:'provincias',
+        component: Provincias,
+        pathMatch: 'full'
+    }, 
+    {
+        path:'cantones',
+        component: Cantones,
+        pathMatch: 'full'
+    }, 
+    {
+        path:'direcciones',
+        component: Direcciones,
+        pathMatch: 'full'
+    }, 
+    
+];
